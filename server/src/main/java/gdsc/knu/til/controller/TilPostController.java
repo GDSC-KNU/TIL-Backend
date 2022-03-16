ebbf2e74f102d9f2eb46259f6e4a2a00dc49ea15
gidskql6671 <gidskql6671@gmail.com>
2022-03-16 20:44:07 +0900

    [Docs] API Docs를 위한 Swagger 세팅 및 적용

--- server/src/main/java/gdsc/knu/til/controller/TilPostController.java
+++ server/src/main/java/gdsc/knu/til/controller/TilPostController.java
@@ -5,6 +5,11 @@ import gdsc.knu.til.dto.TilPostResponse;
 import gdsc.knu.til.exception.InvalidParamException;
 import gdsc.knu.til.exception.TilPostNotFoundException;
 import gdsc.knu.til.service.TilPostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +18,7 @@ import javax.validation.Valid;
 
 
 @RestController
+@Tag(name = "Til Post", description = "Til 게시물 API")
 public class TilPostController {
 
 	private final TilPostService tilPostService;
@@ -21,6 +27,13 @@ public class TilPostController {
 		this.tilPostService = tilPostService;
 	}
 
+	@Tag(name = "Til Post")
+	@Operation(summary = "til 게시글 생성", description = "til 게시글을 생성한다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "201", description = "til 게시글을 정상적으로 만듬. 해당 게시글의 id를 응답으로 보낸다."),
+			@ApiResponse(responseCode = "400", description = "정상적이지 않은 요청 파라미터")
+	})
+	@ResponseStatus(value = HttpStatus.CREATED)
 	@PostMapping("/til-post")
 	public ResponseEntity<Long> create(
 			@Valid @RequestBody TilPostCreateRequest requestDto,
@@ -30,9 +43,16 @@ public class TilPostController {
 			throw new InvalidParamException();
 		}
 
-		return ResponseEntity.ok(tilPostService.create(requestDto));
+		return ResponseEntity.status(201).body(tilPostService.create(requestDto));
 	}
-
+	
+	@Tag(name = "Til Post")
+	@Operation(summary = "til 게시글 상세 조회", description = "til 게시글을 상세 조회한다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "해당 til 게시글의 상세 정보를 응답으로 보낸다."),
+			@ApiResponse(responseCode = "400", description = "부적절한 파라미터 - id 값은 정수이다."),
+			@ApiResponse(responseCode = "404", description = "해당 til 게시글이 존재하지 않는다.")
+	})
 	@GetMapping("/til-post/{id}")
 	public ResponseEntity<TilPostResponse> findById(@PathVariable Long id) {
 		// TODO 로그인 되어있는 유저의 정보를 가져와서 쿼리를 날려야 함. 
--- server/src/main/java/gdsc/knu/til/dto/TilPostCreateRequest.java
+++ server/src/main/java/gdsc/knu/til/dto/TilPostCreateRequest.java
@@ -1,5 +1,6 @@
 package gdsc.knu.til.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
+@Schema(description = "Til 게시물 생성 요청 DTO")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,14 +17,17 @@ import java.time.LocalDate;
 @ToString
 public class TilPostCreateRequest {
 	
+	@Schema(description = "게시글 제목", minLength = 1, maxLength = 50, required = true)
 	@NotBlank(message = "제목이 비어있습니다.")
-	@Size(max=10, message = "최대 길이가 10입니다.")
+	@Size(max=50, message = "최대 길이가 50입니다.")
 	private String title;
 	
+	@Schema(description = "작성하고자 하는 날짜", required = true)
 	@NotNull(message = "날짜가 비어있습니다.")
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
 	private LocalDate date;
 	
+	@Schema(description = "게시글 본문", minLength = 1, maxLength = 10000, required = true)
 	@NotBlank(message = "본문이 비어있습니다.")
 	@Size(max=10000)
 	private String content;
