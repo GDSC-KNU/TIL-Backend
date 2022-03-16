c7a12035cef283189690ddf9a0fa2a3a36a125ed
JiwonHwang <cutty82432@gmail.com>
2022-03-16 21:45:52 +0900

    [feat] 회원가입, 로그인 기능 Controller 추가
    AuthController.java 파일 추가
    HomeController.java 파일 추가

---
+++ server/src/main/java/gdsc/knu/til/controller/AuthController.java
@@ -0,0 +1,31 @@
+package gdsc.knu.til.controller;
+
+
+import gdsc.knu.til.dto.JwtRequestDto;
+import gdsc.knu.til.dto.UserSignupRequestDto;
+import gdsc.knu.til.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = "login", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String longin(@RequestBody JwtRequestDto request){
+        return "login";
+    }
+
+    @PostMapping(value = "signup", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String signup(@RequestBody UserSignupRequestDto request) {
+        return authService.signup(request);
+    }
+
+}
