4c6cdd650cca3756011cb1c1554b3f419318534d
JiwonHwang <cutty82432@gmail.com>
2022-05-16 19:38:28 +0900

    [Refact] Add JPA Auditing

--- server/src/main/java/gdsc/knu/til/TilApplication.java
+++ server/src/main/java/gdsc/knu/til/TilApplication.java
@@ -2,7 +2,9 @@ package gdsc.knu.til;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TilApplication {
 
