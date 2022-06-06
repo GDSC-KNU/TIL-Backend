1264aac17db4349af59301a5a94c290d00d298df
gidskql6671 <gidskql6671@gmail.com>
2022-06-06 13:03:20 +0900

    인증이 필요한 API에 미인증된 유저가 접근했을 때 반환되는 에러 양식 통일

--- src/main/java/gdsc/knu/til/config/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/gdsc/knu/til/config/security/JwtAuthenticationEntryPoint.java
@@ -1,18 +1,33 @@
 package gdsc.knu.til.config.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gdsc.knu.til.exception.ErrorCode;
+import gdsc.knu.til.exception.ErrorResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	
+	private final ObjectMapper objectMapper = new ObjectMapper();
+	private final ErrorResponse errorResponse = ErrorResponse.builder()
+			.code(ErrorCode.UNAUTHORIZED.name())
+			.message(ErrorCode.UNAUTHORIZED.getMessage())
+			.build();
+	
 	@Override
-	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		try (OutputStream os = response.getOutputStream()) {
+			objectMapper.writeValue(os, errorResponse);
+			os.flush();
+		}
 	}
 }
